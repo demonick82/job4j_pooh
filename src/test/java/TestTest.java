4b4b293b026c1128a9688a12c6a67c10ebbb897e
demonick82 <demonick82@gmail.com>
2021-09-03 22:41:41 +0300

    travis

--- src/test/java/TestTest.java
+++ src/test/java/TestTest.java
@@ -1,10 +1,11 @@
 import junit.framework.TestCase;
 import org.junit.Test;
 
-public class TestTest extends TestCase {
-    @Test
-    public void test() {
-        assertEquals(true,true);
-    }
+import static org.junit.Assert.assertTrue;
 
+public class TestTest {
+    @Test
+    public void test11() {
+        assertTrue( true);
+    }
 }
