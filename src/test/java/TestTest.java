b909eaabc2fb2e72782ef26bc892ec3a0c47a439
demonick82 <demonick82@gmail.com>
2021-09-03 22:35:20 +0300

    travis

---
+++ src/test/java/TestTest.java
@@ -0,0 +1,10 @@
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class TestTest extends TestCase {
+    @Test
+    public void test() {
+        assertEquals(true,true);
+    }
+
+}
